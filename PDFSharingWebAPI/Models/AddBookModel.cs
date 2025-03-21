f4dd86aa506745c54615224f7f8b3ebea1bc7d35
Xiao.ZY <2966094090@qq.com>
2025-03-21 20:05:02 +0800

    Add project files.

---
+++ PDFSharingWebAPI/Models/AddBookModel.cs
@@ -0,0 +1,14 @@
+﻿namespace PDFSharingWebAPI.Models;
+
+public class AddBookRequest
+{
+    public required String Name { get; set; }
+    public String? Description { get; set; }
+    public required IFormFile FileData { get; set; }
+}
+
+public class AddBookResponse
+{
+    public required String Result { get; set; }
+    public String? Message { get; set; }
+}
