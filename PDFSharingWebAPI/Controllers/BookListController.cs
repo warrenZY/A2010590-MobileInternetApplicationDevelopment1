7b5f656cac1941cf530bba7dddfa97721d9d92c5
Xiao.ZY <2966094090@qq.com>
2025-03-21 20:05:02 +0800

    Add project files.

---
+++ PDFSharingWebAPI/Controllers/BookListController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PDFSharingWebAPI.Models;
+namespace PDFSharingWebAPI.Controllers;
+
+
+[ApiController]
+[Route("api/[controller]")]
+public class BookListController : ControllerBase
+{
+    // POST
+    [HttpPost]
+    public AddBookResponse Post([FromBody] AddBookRequest request)
+    {
+        Console.WriteLine(request.Name);
+        AddBookResponse response = new()
+        {
+            Message = string.Empty,
+            Result = string.Empty
+        };
+
+        if (request.FileData != null && request.FileData.Length > 0)
+        {
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "pdf", request.Name);
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                request.FileData.CopyTo(stream);
+            }
+            response.Result = "Success";
+        }
+        else
+        {
+            response.Result = "Failure";
+            response.Message = "Failed uploading file to server";
+        }
+        return response;
+    }
+}
---
+++ PDFSharingWebAPI/Program.cs
@@ -0,0 +1,36 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.PropertyNamingPolicy = null;
+});
+
+builder.Services.AddCors(options => {
+        options.AddPolicy("AllowAll", policy => {
+            policy.AllowAnyOrigin()
+                  .AllowAnyMethod()
+                  .AllowAnyHeader();
+        });
+    });
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+app.UseCors("AllowAll");
+
+app.MapControllers();
+
+app.Run();
